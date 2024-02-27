bcf8901f3c64069c2a01fa9146cb3e2597a759a9
KotipalliVenkatasaikumar <147800977+KotipalliVenkatasaikumar@users.noreply.github.com>
2024-02-27 17:58:48 +0530

    save

--- Leave-service/src/main/java/com/leave/LeaveServiceApplication.java
+++ Leave-service/src/main/java/com/leave/LeaveServiceApplication.java
@@ -1,12 +1,12 @@
 package com.leave;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
  
 
 @SpringBootApplication
@@ -16,6 +16,11 @@ public class LeaveServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LeaveServiceApplication.class, args);
 	}
+
+	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+		converters.add(new MappingJackson2HttpMessageConverter());
+	}
+}
 	
 
-}
+
